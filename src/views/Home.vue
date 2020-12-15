63f94164e5bf06e2f600ecefcdd3e754a88854a5
hansh <747263288@qq.com>
2020-12-15 09:57:15 +0800

    fix

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <van-button type="primary">home</van-button>
+    <div>test</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+export default {
+  name: "Home",
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="less">
+  .home{
+    font-size: 0.24rem;
+  }
+</style>
